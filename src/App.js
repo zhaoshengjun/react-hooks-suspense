28e76ed9b4eb45050a2053f8b2a474d4944904ee
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-28 22:54:21 +1000

    refactor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
+  const increment = () => setCount(count + 1);
   return (
     <div className="container">
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <button onClick={increment}>{count}</button>
     </div>
   );
 };
