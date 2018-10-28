6ce0d2acf52326eddb09b919cbb53e577186f0b0
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-28 23:04:57 +1000

    test component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Counter from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("should increment", () => {
+  const { container } = render(<Counter />);
+  const button = container.firstChild.firstChild;
+  expect(button.textContent).toBe("0");
+  fireEvent.click(button);
+  expect(button.textContent).toBe("1");
 });
