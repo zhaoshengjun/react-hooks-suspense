478eac58f8f1640cf8a383916eafa62ec076ff38
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 22:18:58 +1000

    use named variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-const useCounter = (initialState, step) => {
+const useCounter = ({ initialState, step }) => {
   const [count, setCount] = useState(initialState);
   const increment = () => setCount(count + step);
   return { count, increment };
 };
 
 const Counter = () => {
-  const { count, increment } = useCounter(5, 3);
+  const { count, increment } = useCounter({ initialState: 5, step: 3 });
   return (
     <div className="container">
       <button onClick={increment}>{count}</button>
