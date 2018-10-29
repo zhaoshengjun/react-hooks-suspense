a6fa77702bcec92be300049fec178092dc0c363f
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 22:35:42 +1000

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const useCounter = ({ initialState, step }) => {
   const [count, setCount] = useState(initialState);
   const increment = () => setCount(count + step);
-  return { count, increment, stepCount };
+  return { count, increment, setCount };
 };
 
 const Counter = () => {
   const { count, increment } = useCounter({ initialState: 5, step: 3 });
+  useEffect(() => {
+    window.localStorage.setItem("count", count);
+  });
   return (
     <div className="container">
       <button onClick={increment}>{count}</button>
