78df0d8fc8753d8037587a9ad5417ad9d05438dd
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 22:37:33 +1000

    get initial state from localStorage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const useCounter = ({ initialState, step }) => {
 };
 
 const Counter = () => {
-  const { count, increment } = useCounter({ initialState: 5, step: 3 });
+  const initialState = parseInt(window.localStorage.getItem("count")) || 5;
+  const { count, increment } = useCounter({ initialState, step: 3 });
   useEffect(() => {
     window.localStorage.setItem("count", count);
   });
