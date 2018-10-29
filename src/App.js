4d33b27bb63c58979ed53abbb699a636dfeb5e65
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 23:13:06 +1000

    add test

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ const useCounter = ({ initialState, step }) => {
 };
 
 const Counter = () => {
-  const initialState = () =>
-    parseInt(window.localStorage.getItem("count")) || 5;
-  const { count, increment } = useCounter({ initialState, step: 3 });
+  const initialVal = () => parseInt(window.localStorage.getItem("count")) || 0;
+
+  const { count, increment } = useCounter({
+    initialState: initialVal(),
+    step: 1
+  });
+
   useEffect(
     () => {
       window.localStorage.setItem("count", count);
--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,22 @@ import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import Counter from "./App";
 
+afterEach(() => {
+  window.localStorage.removeItem("count");
+});
+
 it("should increment", () => {
   const { container } = render(<Counter />);
   const button = container.firstChild.firstChild;
   expect(button.textContent).toBe("0");
   fireEvent.click(button);
   expect(button.textContent).toBe("1");
+});
+
+it("reads and updates localStorage", () => {
+  window.localStorage.setItem("count", 3);
+  const { container } = render(<Counter />);
+  const button = container.firstChild.firstChild;
+  console.log(button.textContent);
+  expect(button.textContent).toBe("3");
 });
