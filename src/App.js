17c960eeb82948bca6d16da3fe7f6904741fd7b4
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 22:49:15 +1000

    performance refactor

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ const useCounter = ({ initialState, step }) => {
 };
 
 const Counter = () => {
-  const initialState = parseInt(window.localStorage.getItem("count")) || 5;
+  const initialState = () =>
+    parseInt(window.localStorage.getItem("count")) || 5;
   const { count, increment } = useCounter({ initialState, step: 3 });
-  useEffect(() => {
-    window.localStorage.setItem("count", count);
-  });
+  useEffect(
+    () => {
+      window.localStorage.setItem("count", count);
+    },
+    [count]
+  );
   return (
     <div className="container">
       <button onClick={increment}>{count}</button>
