bc446508b527266a40f8475a9c8e280b521cca8a
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-29 22:18:21 +1000

    accept variables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-const useCounter = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
+const useCounter = (initialState, step) => {
+  const [count, setCount] = useState(initialState);
+  const increment = () => setCount(count + step);
   return { count, increment };
 };
 
 const Counter = () => {
-  const { count, increment } = useCounter();
+  const { count, increment } = useCounter(5, 3);
   return (
     <div className="container">
       <button onClick={increment}>{count}</button>
