790e1e353999557ae89089f0ade741913d340d37
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-30 22:16:20 +1000

    useReducer

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,8 +1,33 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useReducer, useRef, useEffect } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LAPSE":
+      return {
+        ...state,
+        lapse: action.now - action.startTime
+      };
+    case "TOGGLE_RUNNING":
+      return {
+        ...state,
+        running: !running
+      };
+    case "CLEAR":
+      return {
+        ...state,
+        lapse: 0,
+        running: false
+      };
+    default:
+      return state;
+  }
+};
 
 function StopWatch() {
-  const [lapse, setLapse] = useState(0);
-  const [running, setRunning] = useState(false);
+  const [{ lapse, running }, dispatch] = useReducer(reducer, {
+    lapse: 0,
+    running: false
+  });
   const intervalRef = useRef(null);
 
   useEffect(() => {
@@ -15,16 +40,15 @@ function StopWatch() {
     } else {
       const startTime = Date.now() - lapse;
       intervalRef.current = setInterval(() => {
-        setLapse(Date.now() - startTime);
+        dispatch({ type: "LAPSE", now: Date.now(), startTime });
       }, 0);
     }
-    setRunning(!running);
+    dispatch({ type: "TOGGLE_RUNNING" });
   };
 
   const handleClearClick = () => {
     clearInterval(intervalRef.current);
-    setLapse(0);
-    setRunning(false);
+    dispatch({ type: "CLEAR" });
   };
   return (
     <div style={{ textAlign: "center" }}>
