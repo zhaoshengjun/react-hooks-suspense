541312d5f3aa99b41aed7f46cf8dc8b72e927642
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-30 21:49:40 +1000

    useState and useRef

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 function StopWatch() {
+  const [lapse, setLapse] = useState(0);
+  const [running, setRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const handleRunClick = () => {
+    if (running) {
+      clearInterval(intervalRef.current);
+    } else {
+      const startTime = Date.now() - lapse;
+      intervalRef.current = setInterval(() => {
+        setLapse(Date.now() - startTime);
+      }, 0);
+    }
+    setRunning(!running);
+  };
+
+  const handleClearClick = () => {};
   return (
     <div style={{ textAlign: "center" }}>
-      <label style={{ fontSize: "5em", display: "block" }}>0ms</label>
-      <button>Start</button>
-      <button>Clear</button>
+      <label style={{ fontSize: "5em", display: "block" }}>
+        {lapse}
+        ms
+      </label>
+      <button onClick={handleRunClick}>{running ? "Stop" : "Start"}</button>
+      <button onClick={handleClearClick}>Clear</button>
     </div>
   );
 }
