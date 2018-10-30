ae70426c1b4739de38fa82f08421f91c4164f7b7
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-30 22:38:44 +1000

    reuse logic

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -4,7 +4,7 @@ const reducer = (currentState, newState) => {
   return { ...currentState, ...newState };
 };
 
-function StopWatch() {
+const useStopWatch = () => {
   const [{ lapse, running }, setState] = useReducer(reducer, {
     lapse: 0,
     running: false
@@ -31,14 +31,33 @@ function StopWatch() {
     clearInterval(intervalRef.current);
     setState({ lapse: 0, running: false });
   };
+
+  return { handleRunClick, handleClearClick, lapse, running };
+};
+
+function StopWatch() {
+  const watchOne = useStopWatch();
+  const watchTwo = useStopWatch();
   return (
     <div style={{ textAlign: "center" }}>
       <label style={{ fontSize: "5em", display: "block" }}>
-        {lapse}
+        {watchOne.lapse}
         ms
       </label>
-      <button onClick={handleRunClick}>{running ? "Stop" : "Start"}</button>
-      <button onClick={handleClearClick}>Clear</button>
+      <button onClick={watchOne.handleRunClick}>
+        {watchOne.running ? "Stop" : "Start"}
+      </button>
+      <button onClick={watchOne.handleClearClick}>Clear</button>
+      <hr />
+      <hr />
+      <label style={{ fontSize: "5em", display: "block" }}>
+        {watchTwo.lapse}
+        ms
+      </label>
+      <button onClick={watchTwo.handleRunClick}>
+        {watchTwo.running ? "Stop" : "Start"}
+      </button>
+      <button onClick={watchTwo.handleClearClick}>Clear</button>
     </div>
   );
 }
