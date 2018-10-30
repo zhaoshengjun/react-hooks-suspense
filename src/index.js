0d06293741199a256243ba5a24c06e15f30cb55b
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-30 23:02:32 +1000

    upper componnent

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-import StopWatch from "./StopWatch";
+// import StopWatch from "./StopWatch";
+import App from "./Upper";
 
-ReactDOM.render(<StopWatch />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
