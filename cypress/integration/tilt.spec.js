452b603787836835bdfcf1551b48cf170c361869
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-03 23:32:47 +1000

    more test

--- cypress/integration/tilt.spec.js
+++ cypress/integration/tilt.spec.js
@@ -6,4 +6,10 @@ context("Tilt", () => {
     cy.title().should("contains", "React");
     cy.get("input[type=checkbox]").should("be.exist");
   });
+
+  it("the button should clickable", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("input[type=checkbox]").click();
+    cy.contains("vanilla-tilt.js").should("exist");
+  });
 });
